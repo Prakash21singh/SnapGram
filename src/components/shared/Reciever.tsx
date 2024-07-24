9b1bfb9e65f41288db395ab139b1b647821433bc
Prakash kumar singh <Pk87096639@gmail.com>
2024-07-24 12:16:02 +0530

    Created

--- src/components/shared/Reciever.tsx
+++ src/components/shared/Reciever.tsx
@@ -1,12 +1,19 @@
 const Reciever = ({ message }: { message: any }) => {
+  const formatTime = (dateString: string) => {
+    const date = new Date(dateString);
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    const strMinutes = minutes < 10 ? "0" + minutes : minutes; // Add leading zero to minutes if needed
+    return `${hours}:${strMinutes} ${ampm}`;
+  };
+
   return (
     <li className="reciever">
       {message?.message}
-      <div className="time">{`${
-        new Date(message.$createdAt).getHours() - 12
-      }:${new Date(message.$createdAt).getHours()} ${
-        new Date(message.$createdAt).getHours() > 12 ? "PM" : "AM"
-      }`}</div>
+      <div className="time">{formatTime(message.$createdAt)}</div>
     </li>
   );
 };
